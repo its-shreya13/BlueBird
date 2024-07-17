d635960af3ebcab1e0bc451d24348e11859a050c
its-shreya13 <shreyamk1303@gmail.com>
2024-07-17 13:52:37 +0530

    new changes

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 const app = express();
 const path = require('path');
 
-mongoose.connect("mongodb://localhost:27017/Mental_Health")
+mongoose.connect("mongodb://localhost:27017/your-db-name")
 .then(()=>{
     console.log('mongoose connected');
 })
