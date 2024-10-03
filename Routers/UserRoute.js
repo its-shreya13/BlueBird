59a41ff7e3c42a3a3141b065c05790f260072c9e
its-shreya13 <shreyamk1303@gmail.com>
2024-10-03 18:59:11 +0530

    updated

--- Routers/UserRoute.js
+++ Routers/UserRoute.js
@@ -1,12 +1,1 @@
 
-app.get('/', (req, res) => {
-   res.sendFile(path.join('home.html'));
-});
-
-app.get('/register', (req, res) => {
-   res.sendFile(path.join( 'Sign-in.html'));
-});
-
-app.get('/login', (req, res) => {
-   res.sendFile(path.join('Log-in.html'));
-});
